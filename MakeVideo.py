5ee418d91e50e28a7784db897c14326291ce2ffe
CBUnmanned <103763456+CBUnmanned@users.noreply.github.com>
2022-12-28 22:40:21 +0000

    Create MakeVideo.py

---
+++ MakeVideo.py
@@ -0,0 +1,16 @@
+# Use ffmpeg to encode into a video file
+
+import os
+
+currentDir = os.path.abspath(os.getcwd())
+targetFolder = input("targetFolder: ")
+targetName = input("targetName: ")
+framerate = input("frame rate: ")
+
+print(currentDir)
+
+def save():
+    os.system('ffmpeg -start_number 0 -r {} -i {}\{}\%d.png -vcodec mpeg4 -y {}\{}.mp4'.format(framerate, currentDir, targetFolder,  targetFolder, targetName))
+save()
+
+
